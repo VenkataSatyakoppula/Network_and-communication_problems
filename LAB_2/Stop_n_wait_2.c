9474247588f40d16edbc6d3c5cef3d0ca7fe858c
VenkataSatyakoppula <60220673+VenkataSatyakoppula@users.noreply.github.com>
2021-04-12 18:48:19 +0530

    stop_n_wait_2 updated

--- LAB_2/Stop_n_wait_2.c
+++ LAB_2/Stop_n_wait_2.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<time.h>
+#include<stdlib.h>
 int s=1,r=1,ack=0;
 int frames[50],n;
 int timeout ( int seconds )
@@ -9,8 +10,16 @@ int timeout ( int seconds )
     while (clock() < endwait) {}
     return  1;
 }
-
+int random(int lower, int upper)
+{
+    int num = rand() % upper;
+    return num;
+}
 int main()
 {
+    srand(time(0));
+    int num=random(1,10);
+    timeout(num);
+    printf("satya %d",num);
     
 }

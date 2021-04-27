f23ab71ed486002f14a1f7d5774dcf5264391af6
VenkataSatyakoppula <satya5202@gmail.com>
2021-04-27 08:23:22 +0530

    LAB_3 Q_1

---
+++ LAB_3/IP_Address.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+
+
+int main(){
+    char IP[16];
+    printf("Enter the IP Address in decimal form(xxx.xxx.xxx.xxx):");
+    scanf("%s",IP);
+    printf("%s",IP);
+
+}
